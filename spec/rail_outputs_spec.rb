14edccbef59893269c0535ac159cac2e070d31ad
Andrii Baran <andriy.baran.v@gmail.com>
2021-01-08 14:24:11 +0200

    Updated mature_factory gem dependency version. Added specs

--- spec/rail_outputs_spec.rb
+++ spec/rail_outputs_spec.rb
@@ -141,5 +141,68 @@ RSpec.describe FlowObject::Base do
         expect(operation.output).to have_attributes(step: :authorize_stage, error:'Error with id=3')
       end
     end
+
+    context 'when something went wrong in inputs' do
+      vars do
+        operation_class do
+          Class.new(FlowObject::Base) do
+            from :mash
+            to :json
+            input :mash, base_class: Struct.new(:id), init: ->(klass, value) { klass.new(value) }
+            output :rake, base_class: OpenStruct
+            output :json
+            flow do
+              stage :authorize
+              stage :sync
+              stage :store
+              stage :formatter
+            end
+            mash_input do
+              def a; 'a'; end
+              def valid?; false; end
+
+              def errors
+                ['Error with id=3']
+              end
+            end
+            authorize_stage do
+              def o; 'o'; end
+              def valid?; false; end
+            end
+            sync_stage do
+              def u; 'u'; end
+            end
+            store_stage do
+              def e; 'e'; end
+            end
+            formatter_stage do
+              def i; 'i'; end
+            end
+            json_output do
+              attr_accessor :error, :step
+              def on_mash_input_failure(output)
+                self.error = output.errors.join
+                self.step = :mash_input
+              end
+            end
+            rake_output do
+              def on_failure(given, step)
+                self.error = given.errors.join
+              end
+            end
+
+            def on_authorize_stage_failure
+              output.errors.join
+            end
+          end
+        end
+        value { 3 }
+      end
+
+      it 'calls on_authorize_failure of :json output' do
+        operation = operation_class.accept(value).call
+        expect(operation.output).to have_attributes(step: :mash_input, error:'Error with id=3')
+      end
+    end
   end
 end
